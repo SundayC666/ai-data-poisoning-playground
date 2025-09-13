d9517640177c51c7aeb78f1356ca8aa21b15eedc
Sunday Chen <sandechen1998@gmail.com>
2025-09-13 18:35:45 -0400

    revise file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base:'/ai-poisoning-playground',
+  base:'/ai-poisoning-playground/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
