15e199731a79218d33b6d1a52443b85bad1fddf5
Sunday Chen <sandechen1998@gmail.com>
2025-09-13 17:50:59 -0400

    revise hash router and base

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
 import DemoView from '../views/DemoView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
     {
       path: '/',
       name: 'ai-demo',
